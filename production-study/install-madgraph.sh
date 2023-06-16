7d4c40fba844259ebab5ae6307ec9f9d2055dfa2
SiewYan <siew.yan.hoh@gmail.com>
2023-06-16 13:27:56 +0800

    update ggF

--- production-study/install-madgraph.sh
+++ production-study/install-madgraph.sh
@@ -2,5 +2,5 @@
 
 wget https://launchpad.net/mg5amcnlo/3.0/3.5.x/+download/MG5aMC_LTS_2.9.15.tgz
 tar zxvf MG5aMC_LTS_2.9.15.tgz
-cd MG5_aMC_v2_9_15
-./bin/mg5_aMC ../madgraph.txt
+#cd MG5_aMC_v2_9_15
+#./bin/mg5_aMC ../madgraph.txt
